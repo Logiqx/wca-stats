a34e97552a01a14112564bb644805fbfb54b9677
Logiqx <software@mikeg.me.uk>
2019-04-18 10:35:06 +0100

    Add MultiBLD attempts

---
+++ sql/multibld_attempts.sql
@@ -0,0 +1,38 @@
+/* 
+    Script:   MultiBLD attempts
+    Created:  2019-04-18
+    Author:   Michael George / 2015GEOR02
+   
+    Purpose:  Request by Mark Adams to determine the best strategy for 9 points
+
+              Do more people get 10/11, 11/11 or 9/9?
+              Do you have it as a percentage of those who have attempted 11 and 9?
+*/
+
+/*
+    Explanation of MBLD results
+    
+    best          = 0DDTTTTTMM
+    points        = 99 - DD
+    missed        = MM
+    solved        = difference + missed
+    attempted     = solved + missed
+    timeInSeconds = TTTTT
+*/
+
+SELECT attempted, solved, missed, points, num_persons,
+    FORMAT(100.0 * num_persons / SUM(num_persons) OVER(PARTITION BY attempted), 2) AS pct_persons
+FROM 
+(
+    SELECT
+        99 - FLOOR(best / 10000000) + (best % 100) * 2 AS attempted,
+        99 - FLOOR(best / 10000000) + (best % 100) AS solved,
+        best % 100 AS missed,
+        99 - FLOOR(best / 10000000) AS points,
+        COUNT(*) AS num_persons
+    FROM Results
+    WHERE eventId = '333mbf'
+    AND best % 100 > -1
+    GROUP BY attempted, solved
+) tmp_multi
+ORDER BY attempted, solved DESC;
