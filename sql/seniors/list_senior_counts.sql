59ac527e0f47fe9c9e02c555b8de8fa79d185212
Logiqx <software@mikeg.me.uk>
2019-06-16 15:12:07 +0100

    Break down by age category

--- sql/seniors/list_senior_counts.sql
+++ sql/seniors/list_senior_counts.sql
@@ -6,17 +6,30 @@
     Purpose:  Count the number of people over the age of 40 with competition results
 */
 
-SELECT eventId, COUNT(DISTINCT singleId) AS numSingleIds, COUNT(DISTINCT averageId) AS numAverageIds
+SELECT eventId, resultType, ageCategory, COUNT(*)
 FROM
 (
-  SELECT eventId, personId AS singleId, IF(average > 0, personId, NULL) AS averageId,
-    TIMESTAMPDIFF(YEAR,
+  SELECT eventId, 'single' AS resultType, personId,
+    MAX(FLOOR(TIMESTAMPDIFF(YEAR,
       DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
-      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
+      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) / 10) * 10) AS ageCategory
   FROM Results AS r
   INNER JOIN Competitions AS c ON r.competitionId = c.id
   INNER JOIN Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
-  HAVING age_at_comp >= 40
+  WHERE best > 0
+  GROUP BY eventId, personId
+  HAVING ageCategory >= 40
+  UNION ALL
+  SELECT eventId, 'average' AS resultType, personId,
+    MAX(FLOOR(TIMESTAMPDIFF(YEAR,
+      DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
+      DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) / 10) * 10) AS ageCategory
+  FROM Results AS r
+  INNER JOIN Competitions AS c ON r.competitionId = c.id
+  INNER JOIN Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 40
+  WHERE average > 0
+  GROUP BY eventId, personId
+  HAVING ageCategory >= 40
 ) AS tmp_persons
-GROUP BY eventId
-ORDER BY numSingleIds DESC, numAverageIds DESC;
+GROUP BY eventId, resultType, ageCategory
+ORDER BY eventId, resultType, ageCategory;
