1a4fd8be6d111218b6a2bd81ecee701b01b30fa4
Logiqx <software@mikeg.me.uk>
2019-07-07 20:12:47 +0100

    Re-factor for impproved performance

--- sql/seniors/list_age_bands.sql
+++ sql/seniors/list_age_bands.sql
@@ -12,15 +12,19 @@ FROM
   SELECT DISTINCT personId, FLOOR(age_at_comp / 5) * 5 AS age_band
   FROM
   (
+    -- Derived table contains senior results, including age at the start of the competition
+    -- Index hint (USE INDEX) ensures that MySQL / MariaDB uses the optimal query execution plan
+    -- i.e. Persons ("WHERE" limits to seniors) -> Results.personId -> Competitions.id
     SELECT r.personId,
       TIMESTAMPDIFF(YEAR,
-        DATE_FORMAT(CONCAT(p.year, "-", p.month, "-", p.day), "%Y-%m-%d"),
-        DATE_FORMAT(CONCAT(c.year, "-", c.month, "-", c.day), "%Y-%m-%d")) AS age_at_comp
-    FROM Results AS r
-    INNER JOIN Competitions AS c ON r.competitionId = c.id
-    INNER JOIN Persons AS p ON r.personId = p.id AND p.subid = 1 AND p.year > 0 AND p.year <= YEAR(CURDATE()) - 20
-    WHERE best > 0
-    HAVING age_at_comp >= 20
+        DATE_FORMAT(CONCAT(p.year, '-', p.month, '-', p.day), '%Y-%m-%d'),
+        DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS age_at_comp
+    FROM Persons AS p USE INDEX ()
+    JOIN Results AS r ON r.personId = p.id AND best > 0
+    JOIN Competitions AS c ON c.id = r.competitionId
+    WHERE p.year > 0 AND p.year <= YEAR(CURDATE()) - 30
+    AND p.subid = 1
+    HAVING age_at_comp >= 30
   ) AS tmp_results
 ) AS tmp_persons
 GROUP BY age_band
