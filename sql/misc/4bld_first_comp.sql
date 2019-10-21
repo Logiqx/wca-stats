55f6cf2ebe4df209e6e579cfa95462813c7b54c4
Logiqx <software@mikeg.me.uk>
2019-10-21 12:37:15 +0100

    Fix indentation

--- sql/misc/4bld_first_comp.sql
+++ sql/misc/4bld_first_comp.sql
@@ -10,16 +10,16 @@
 SELECT p.id, p.name, p.countryId, compName, compDate
 FROM
 (
-	SELECT personId, MIN(DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS firstComp
-	FROM Results AS r
-	JOIN Competitions AS c ON c.id = r.competitionId
-	GROUP BY personId
+  SELECT personId, MIN(DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS firstComp
+  FROM Results AS r
+  JOIN Competitions AS c ON c.id = r.competitionId
+  GROUP BY personId
 ) AS t1
 JOIN (
-	SELECT personId, c.name AS compName, DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS compDate
-	FROM Results AS r
-	JOIN Competitions AS c ON c.id = r.competitionId
-    WHERE eventId = '444bf' AND best > 0
+  SELECT personId, c.name AS compName, DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS compDate
+  FROM Results AS r
+  JOIN Competitions AS c ON c.id = r.competitionId
+  WHERE eventId = '444bf' AND best > 0
 ) AS t2 ON t2.personId = t1.personId AND t2.compDate = t1.firstComp
 JOIN Persons AS p ON p.id = t2.personId
 ORDER BY compDate;
