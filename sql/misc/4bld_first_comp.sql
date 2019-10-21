423fcfd8229b659f5654855e0b8a026dee15fd0d
Logiqx <software@mikeg.me.uk>
2019-10-21 16:19:32 +0100

    Add positions

--- sql/misc/4bld_first_comp.sql
+++ sql/misc/4bld_first_comp.sql
@@ -7,7 +7,7 @@
     Link:     https://www.facebook.com/groups/439995439706174/permalink/935796790126034/
 */
 
-SELECT p.id, p.name, p.countryId, compName, compDate
+SELECT p.id, p.name, p.countryId, compName, compDate, round, pos
 FROM
 (
     SELECT personId, MIN(DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d')) AS firstComp
@@ -16,9 +16,10 @@ FROM
     GROUP BY personId
 ) AS t1
 JOIN (
-    SELECT personId, c.name AS compName, DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS compDate
+    SELECT personId, c.name AS compName, DATE_FORMAT(CONCAT(c.year, '-', c.month, '-', c.day), '%Y-%m-%d') AS compDate, rt.name AS round, pos
     FROM Results AS r
     JOIN Competitions AS c ON c.id = r.competitionId
+    JOIN RoundTypes AS rt ON rt.id = r.roundTypeId
     WHERE eventId = '444bf' AND best > 0
 ) AS t2 ON t2.personId = t1.personId AND t2.compDate = t1.firstComp
 JOIN Persons AS p ON p.id = t2.personId
